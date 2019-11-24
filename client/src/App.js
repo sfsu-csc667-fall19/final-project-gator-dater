96ee186ffed1ffea6e81b7775e7c26486b1c2453
Seafoodghost <Seafoodghost@Seafoodghosts-MacBook-Air.local>
2019-11-23 19:24:55 -0800

    Login,Create Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route}  from "react-router-dom";
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
+ 
+  
   return (
-    <div>
-      Hello
-    </div>
+        <Switch>
+          <Route exact path ="/login" component = {Profile}/>
+          <Route exact path ="/" component = {Home}/>
+        </Switch>
   );
 }
-
 export default App;
