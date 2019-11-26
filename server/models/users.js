7475b3e88f4df060f388702134b20b183c0429ae
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2019-11-25 20:49:50 -0800

    additional field for schema

--- server/models/users.js
+++ server/models/users.js
@@ -7,10 +7,13 @@ let usersSchema = new Schema({
     password: String,
     firstName: String,
     lastName: String,
+    email: String,
     age: { type: Number, min: 18, max: 65 },
     race: String,
     gender: String,
-    collegeYear: String,
+    collegeyear: String,
+    major:String,
+    addition:String,
     interests: String,
     
 });
