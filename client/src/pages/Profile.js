edffdba2291bcb1c20dcfc49c828a6af3dbd3648
Raymond W Gee <rgee5@mail.sfsu.edu>
2019-12-08 17:08:43 -0800

    integrated card into the Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -69,81 +69,75 @@ const Profile = ({dispatch, isUpdateOpen}) => {
 
   }
 
-
   if (isLoggedOut) { return <Redirect to="/" /> }
 
-
-  return (
-    
-    <div style={bgGround} >
-    <div id = "main">
-    <Navbar color="warning" light expand="md" >
-        <NavbarBrand >GatorDater- Profile.js</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Some Option#1</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="">Some Option#2</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          {/* <input type="image" src = "./img/fb.jpg" border="border of the image" alt="text"></input> */}
-         Welcome, Seafoodghost &nbsp;&nbsp;&nbsp;
-         <Button  variant="warning" onClick = {openNav}> &#9776;</Button>
-        </Collapse>
-      </Navbar>
+    return (
       
-     
-    
-       
-      <Switch>
-      {isUpdateOpen && ( 
-        <Route path = "/" component ={Update}/> 
+      <div style={bgGround}>
+      <div id = "main">
+      <Navbar color="warning" light expand="md" >
+          <NavbarBrand >GatorDater- Profile.js</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Some Option#1</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">Some Option#2</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            {/* <input type="image" src = "./img/fb.jpg" border="border of the image" alt="text"></input> */}
+          Welcome, Seafoodghost &nbsp;&nbsp;&nbsp;
+          <Button  variant="warning" onClick = {openNav}> &#9776;</Button>
+          </Collapse>
+        </Navbar>
         
-      )}
-        <Route path = "/" component = {Random}/>       
-      </Switch>
-
-
       
-      <div id="mySidenav" className="sidenav"> 
-        <ListGroup>
-         
-          <ListGroupItem>Some information.</ListGroupItem>
-          <ListGroupItem>Email: Some content@sfsu.edu</ListGroupItem>
-          <ListGroupItem>Some content</ListGroupItem>
-          <ListGroupItem>Some content</ListGroupItem>
-          <ListGroupItem><Button bsSize = "sm" onClick ={goUpdate} block>Update Profile</Button></ListGroupItem>
-          <ListGroupItem><Button  bsSize = "sm"  onClick = {logout} block> LogOut</Button></ListGroupItem>
-        </ListGroup>       
-     </div>
-     </div> 
-    </div>
- 
-  );
-};
+      
+        
+        <Switch>
+        {isUpdateOpen && ( 
+          <Route path = "/" component ={Update}/> 
+          
+        )}
+          <Route path = "/" component = {Random}/>       
+        </Switch>
 
 
-
+        
+        <div id="mySidenav" className="sidenav"> 
+          <ListGroup>
+            <ListGroupItem>Some information.</ListGroupItem>
+            <ListGroupItem>Email: Some content@sfsu.edu</ListGroupItem>
+            <ListGroupItem>Some content</ListGroupItem>
+            <ListGroupItem>Some content</ListGroupItem>
+            <ListGroupItem><Button bsSize = "sm" onClick ={goUpdate} block>Update Profile</Button></ListGroupItem>
+            <ListGroupItem><Button bsSize = "sm"  onClick = {logout} block> LogOut</Button></ListGroupItem>
+          </ListGroup>       
+        </div>
+      </div> 
+      </div>
+    );
+  };
+//}
 
 const mapStateToProps = state => ({
    isUpdateOpen: state.pageReducer.isUpdateOpen,
--- client/src/pages/Random.js
+++ client/src/pages/Random.js
@@ -4,15 +4,59 @@ import { Button} from 'react-bootstrap';
 import { Card, Row, Col, CardSubtitle, CardBody,CardTitle} from 'reactstrap';
 import { Label, Form, FormGroup, Input, Alert, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText} from 'reactstrap';
 
+// Raymond's card imports
+import Jedi from './img/Jedi.jpg';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Card2 from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+//import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
+import Fab from '@material-ui/core/Fab';
+
 const Random = () => {
   const [messageBox, setMessageBox] = useState(false);
-
-
 
   const openMessage = () =>{
     setMessageBox(!messageBox);
   }
 
+  // card css style imports for Raymond
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+      marginTop: 20,
+      marginLeft: 20,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+  const styles = {
+    card: {
+      padding: 20,
+    }
+  }
+
+  // styles for the card- Raymond
+  //export default function MediaCard() {
+  const classes = useStyles();
+
+  // handling to toggle heart appearance/color
+  const [heart, setHeart] = React.useState('default');
+  const updateHeart = () => {
+    if(heart==='default') {
+      setHeart('secondary');
+    } else {
+      setHeart('default');
+    }
+  };
 
   const Message = ()=>{      
     return (
@@ -69,6 +113,38 @@ const Random = () => {
             )}     
       </Row>
       
+      
+      <Grid container>
+          {/* Card #1 */}
+          <Grid item sm>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={Jedi}
+                  title="Friendly doge"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Jedi, 12
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Jedi loves going out for long walks and sniffing absolutely everything.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions> {/* Handler example for changing heart color */}
+                <Fab active aria-label="like" color={heart} onClick={() => setHeart(updateHeart)}>
+                  <FavoriteIcon />
+                </Fab>
+                <Fab active aria-label="like" style={{marginLeft: 20}}>
+                  <CancelRoundedIcon />
+                </Fab>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+
     </div>
   );
 };
